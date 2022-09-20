f19407656f37bb224f3c5c6f8233985551539b7a
mooncy0809 <mooncy0809@naver.com>
2022-09-20 17:59:36 +0900

    mcy

--- OJ_1085.py
+++ OJ_1085.py
@@ -1,23 +1,19 @@
-a = list(input())
-b = list(input())
-c = list(input())
-a_cnt_l = []
-a_l = []
-b_cnt_l = []
-b_l = []
-c_cnt_l = []
-c_l = []
-a_cnt = 0
-b_cnt = 0
-c_cnt = 0
-print(a)
-print(b)
-for i in len(a):
-    if a[i]==a[i+1]
-        a_cnt +=1
-        if a[i] != a[i+1]:
-            a_cnt_l.append(a_cnt)
-            a_l.append(a[i])
-            a_cnt = 0
+num_li=[]
+for i in range(3):
+    num_li.append(input())
 
+for n in range(len(num_li)):
+    cnt,mcnt = 0,0
+    a = list(num_li[n])
 
+    for i in range(1,9):
+        if a[i-1] == a[i]:
+            cnt += 1
+            if mcnt < cnt:
+                mcnt = cnt
+        if a[i-1] != a[i]:
+            cnt = 1
+    if mcnt == 0:
+        print(1)
+    else:
+        print(mcnt)
