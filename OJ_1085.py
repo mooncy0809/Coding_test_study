a82d9fff05cd2a02c7ede3e152d216e290089b7f
mooncy0809 <mooncy0809@naver.com>
2022-09-27 17:39:06 +0900

    mcy

--- OJ_1085.py
+++ OJ_1085.py
@@ -1,17 +1,13 @@
-num_li=[]
 for i in range(3):
-    num_li.append(input())
+    a, cnt, mcnt = [], 0, 0
+    a[:] = list(input())
 
-for n in range(len(num_li)):
-    cnt,mcnt = 0,0
-    a = list(num_li[n])
-
-    for i in range(1,9):
-        if a[i-1] == a[i]:
+    for i in range(8):
+        if a[i - 1] == a[i]:
             cnt += 1
             if mcnt < cnt:
                 mcnt = cnt
-        if a[i-1] != a[i]:
+        if a[i - 1] != a[i]:
             cnt = 1
     if mcnt == 0:
         print(1)
