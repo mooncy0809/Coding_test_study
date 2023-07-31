cf242e53f09d2b958cc710ed1ab37510d0cc6f10
mooncy0809 <mooncy0809@naver.com>
2023-07-31 17:33:09 +0900

    백준 시간복잡도

---
+++ 백준/BJ_24262.java
@@ -0,0 +1,17 @@
+package Coding_test_study.백준;
+
+/*
+* 오늘도 서준이는 알고리즘의 수행시간 수업 조교를 하고 있다. 아빠가 수업한 내용을 학생들이 잘 이해했는지 문제를 통해서 확인해보자.
+입력의 크기 n이 주어지면 MenOfPassion 알고리즘 수행 시간을 예제 출력과 같은 방식으로 출력해보자.
+MenOfPassion 알고리즘은 다음과 같다.
+MenOfPassion(A[], n) {
+    i = ⌊n / 2⌋;
+    return A[i]; # 코드1
+}
+* */
+public class BJ_24262 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
---
+++ 백준/BJ_24263.java
@@ -0,0 +1,12 @@
+package Coding_test_study.백준;
+
+import java.util.Scanner;
+
+public class BJ_24263 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
