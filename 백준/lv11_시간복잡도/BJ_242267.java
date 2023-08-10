6b3c2b2acebbca84b35ff917cd8b10d43364fa30
mooncy0809 <mooncy0809@naver.com>
2023-08-10 22:53:47 +0900

    시간복잡도

--- 백준/lv11_시간복잡도/BJ_242267.java
+++ 백준/lv11_시간복잡도/BJ_242267.java
@@ -5,5 +5,7 @@ import java.util.Scanner;
 public class BJ_242267 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        System.out.println((n*(n-1)*(n-2)/6)+"\n" + 3);
     }
 }
